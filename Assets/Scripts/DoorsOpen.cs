55b2733c2d43889d60d85a03817e727f893c0d45
marinap20 <marinapop23@gmail.com>
2023-03-25 23:02:22 -0400

    new script
    started

---
+++ Assets/Scripts/DoorsOpen.cs
@@ -0,0 +1,27 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class DoorsOpen : MonoBehaviour
+//{
+//    GameObject pacman;
+//    GameObject doors;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    { 
+
+//        int p1x = (int)pacman.transform.position.x;
+//        int p1y = (int)pacman.transform.position.y;
+//        int d1x = (int)
+
+//        if (Mathf.Abs(p1x - ))
+
+//    }
+//}
