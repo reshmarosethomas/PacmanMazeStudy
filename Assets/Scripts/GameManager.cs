e5ebb464f24f7552a4bcf46bca5e53d498605ae6
marinap20 <marinapop23@gmail.com>
2023-03-26 11:04:58 -0400

    Signal added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -132,6 +132,7 @@ public class GameManager : MonoBehaviour
             Tinylytics.AnalyticsManager.LogCustomMetric(SaveProlificID.prolificID + "_" + tempTrialName + "_" + tempTrialNum.ToString() + "_" + "ClydeDistances", clydeDists);
 
             //4. No of Times Pacman passed through a door (in C2, C3)
+            Tinylytics.AnalyticsManager.LogCustomMetric(SaveProlificID.prolificID + "_" + tempTrialName + "_" + tempTrialNum.ToString() + "_" + "DoorOpenNum", open_door_num);
 
 
             //5. ExitedFullScreen?
--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -12,9 +12,10 @@ public class OpenDoor : MonoBehaviour
 
     SpriteRenderer doorRenderer;
 
-    public float sensorStrength = 3;
+    public float sensorStrength = 1;
 
     float distPacman = 0f, distBlinky = 0f, distInky = 0f, distPinky = 0f, distClyde =0f;
+    float door_open_num = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -37,13 +38,28 @@ public class OpenDoor : MonoBehaviour
         distPinky = Vector2.Distance(pinkyPos.position, transform.position);
         distClyde = Vector2.Distance(clydePos.position, transform.position);
 
-        if (distPacman < sensorStrength || distBlinky < sensorStrength || distInky < sensorStrength || distPinky < sensorStrength || distClyde < sensorStrength)
+        if (distBlinky < sensorStrength || distInky < sensorStrength || distPinky < sensorStrength || distClyde < sensorStrength)
         {
             Debug.Log("DistAlert");
             Color tempDoorColor = doorRenderer.color;
             tempDoorColor.a = 0f;
             doorRenderer.color = tempDoorColor;
         } else
+        {
+            Color tempDoorColor = doorRenderer.color;
+            tempDoorColor.a = 1f;
+            doorRenderer.color = tempDoorColor;
+        }
+
+        if (distPacman < sensorStrength)
+        {
+            Debug.Log("DistAlert");
+            Color tempDoorColor = doorRenderer.color;
+            tempDoorColor.a = 0f;
+            doorRenderer.color = tempDoorColor;
+            door_open_num += 1f;
+        }
+        else
         {
             Color tempDoorColor = doorRenderer.color;
             tempDoorColor.a = 1f;
