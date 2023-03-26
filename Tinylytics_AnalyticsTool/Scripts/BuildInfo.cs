e78ebb9aea1ef2a8c4c2605b96bd2f6cf367336e
reshmarosethomas <reshmarosethomas@gmail.com>
2023-03-25 23:53:10 -0400

    Added Tinylytics Files (I think)

---
+++ Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -0,0 +1,1 @@
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "2/26/2023 12:36:16 AM";}}
---
+++ Tinylytics_AnalyticsTool/_DemoScene/SendDataViaCallInScript_Example.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Tinylytics.Examples {
+
+	/// <summary>
+	/// This example shows how to call the Tinylytics AnalyticsManager directly via a static call, without using the widget.
+	/// </summary>
+	public class SendDataViaCallInScript_Example : MonoBehaviour {
+
+
+		void Start() {
+			Tinylytics.AnalyticsManager.LogCustomMetric("Current Month", System.DateTime.Now.Month.ToString());
+		}
+
+	}
+}
