5a5820fd846829befd6528023d83d2b0b4a1fb13
reshmarosethomas <reshmarosethomas@gmail.com>
2023-03-27 01:03:44 -0400

    Compression Settings

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/26/2023 9:26:15 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/27/2023 12:50:38 AM";}}
