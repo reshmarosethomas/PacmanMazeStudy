7e69de7338c022634a21646fbfa9425608c243ed
marinap20 <marinapop23@gmail.com>
2023-03-27 09:23:40 -0400

    changed conditions for study 2

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/27/2023 5:45:46 AM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/27/2023 7:06:33 AM";}}
