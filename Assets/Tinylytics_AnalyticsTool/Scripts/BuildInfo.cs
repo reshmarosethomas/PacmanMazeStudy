769294e82746c210ad214b578282d6a0b5468ef9
marinap20 <marinapop23@gmail.com>
2023-03-27 05:50:51 -0400

    fixed instructions _ version up on marina's itch.io account

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/27/2023 12:50:38 AM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "3/27/2023 5:45:46 AM";}}
